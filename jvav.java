cd51085979f66186491ea7bb6652515cb0bd0126
Packed ice <112376986+Packice@users.noreply.github.com>
2023-08-18 18:35:44 +0800

    Create jvav.java

---
+++ jvav.java
@@ -0,0 +1,33 @@
+package com.jvav.code;
+
+public class jvav{
+
+    public static void shuo (String language){
+        System.out.println(language);
+        /*可以来说话*/
+    }
+    public static void jia (int matha,int mathb){
+        /*做加法*/
+        int out;
+        out=matha+mathb;
+        System.out.println(out);
+
+    }
+    public static void jian (int matha,int mathb){
+        /*做减法*/
+        int out;
+        out=matha-mathb;
+        System.out.println(out);
+    }
+ /*
+    public static void mu(int matha,int mathb){
+        
+        while(mathb==0){
+        int outmath=matha*matha;
+        mathb=mathb-1;
+        }
+        System.out.println(out);
+    }
+    */
+    
+}
