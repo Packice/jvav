d71354af8ad9d3435588e0eef9af6408e411d119
Packed ice <112376986+Packice@users.noreply.github.com>
2023-10-01 11:06:24 +0800

    Update jvav.java

--- jvav.java
+++ jvav.java
@@ -1,12 +1,13 @@
 package com.jvav.code;
+import java.util.*;
 
 public class jvav{
 
-    public static void shuo (String language){
+    public static void say (String language){
         System.out.println(language);
         /*可以来说话*/
     }
-    public static void jia (int matha,int mathb){
+    public static void and (int matha,int mathb){
         /*做加法*/
         int out;
         out=matha+mathb;
@@ -19,15 +20,24 @@ public class jvav{
         out=matha-mathb;
         System.out.println(out);
     }
- /*
-    public static void mu(int matha,int mathb){
-        
-        while(mathb==0){
-        int outmath=matha*matha;
-        mathb=mathb-1;
-        }
+ 
+    public static void mu(double matha,double mathb){
+        /*幂运算*/
+        double out = Math.pow(matha,mathb);
         System.out.println(out);
     }
-    */
+    /*public static void line (int in,int in2)
+    {
+        
+        while(true){
+            while(in == 0){
+            System.out.println("―");
+            in --;
+            }
+        System.out.println("\n");
+        in2 --;
+        }
+        
+    }*/
     
 }
